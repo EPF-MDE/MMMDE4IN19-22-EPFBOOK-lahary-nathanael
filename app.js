cd295d1b79a1210287fd902e5d71f0daedcf0d29
nlahary <nathanaellahary@gmail.com>
2024-04-02 10:42:29 +0200

    tp4

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
 const fs = require("fs")
 const path = require("path"); // Require the path module
+const basicAuth = require('express-basic-auth')
+
+const token = "FOOBAR";
+const tokenCookie = {
+  path: "/",
+  httpOnly: true,
+  expires: new Date(Date.now() + 60 * 60 * 1000),
+};
+res.cookie("auth-token", token, tokenCookie);
+
+
+const customAuthorizer  = (username, password, cb) => {
+  if (!username || !password) {
+    return cb(new Error("Username or password were not defined"), false);
+  }
+  // Parse the CSV file: this is very similar to parsing students!
+  parseCsvWithHeader("./users.csv", (err, users) => {
+    console.log(users);
+    // Check that our current user belong to the list
+    const storedUser = users.find((possibleUser) => {
+      if (!possibleUser.username) {
+        console.warn(
+          "Found a user with no username in users.csv",
+          possibleUser
+        );
+        return false;
+      }
+      // NOTE: a simple comparison with === is possible but less safe
+      return basicAuth.safeCompare(username, possibleUser.username);
+    });
+
+    if (!storedUser) {
+      cb(null, false);
+    } else if (!storedUser.password) {
+      console.warn(
+        "Found a user with no password in users.csv",
+        storedUser
+      );
+      cb(null, false);
+    } else if (!basicAuth.safeCompare(password, storedUser.password)) {
+      cb(null, false);
+    } else {
+      // success: user is found and have the right password
+      cb(null, true);
+    }
+  });
+};
+
+
+app.use(basicAuth({
+  authorizer: customAuthorizer,
+  challenge: true
+}))
+
 app.use(express.urlencoded({ extended: true })); // This line enables parsing of URL-encoded data
 
 app.set('views','./views'); 
@@ -47,6 +102,19 @@ app.post("/students/create", (req, res) => {
 
 // 
 
+app.post("/api/login", (req, res) => {
+  console.log("current cookies:", req.cookies);
+  // We assume that you check if the user can login based on "req.body"
+  // and then generate an authentication token
+  const token = "FOOBAR";
+  const tokenCookie = {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 60 * 1000),
+  };
+  res.cookie("auth-token", token, tokenCookie);
+  res.send("OK");
+});
 
 app.get('/students', (req, res) => {
   // Read student data from the CSV file
@@ -118,6 +186,30 @@ app.get('/api/students', (req, res) => {
 
 
 });
+
+
+const parseCsvWithHeader = (filepath, cb) => {
+  const rowSeparator = "\n";
+  const cellSeparator = ",";
+  // example based on a CSV file
+  fs.readFile(filepath, "utf8", (err, data) => {
+    const rows = data.split(rowSeparator);
+    // first row is an header I isolate it
+    const [headerRow, ...contentRows] = rows;
+    const header = headerRow.split(cellSeparator);
+
+    const items = contentRows.map((row) => {
+      const cells = row.split(cellSeparator);
+      const item = {
+        [header[0]]: cells[0],
+        [header[1]]: cells[1],
+      };
+      return item;
+    });
+    return cb(null, items);
+  });
+};
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} test`)
